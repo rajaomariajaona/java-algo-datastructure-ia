f98b46e83e4f27fbb3d2f8b25339f8438fdce6f9
rajaomariajaona <rajaomariajaona11@gmail.com>
2021-03-28 18:28:18 +0300

    basic javafx

--- app/src/main/java/mg/emit/App.java
+++ app/src/main/java/mg/emit/App.java
@@ -3,12 +3,29 @@
  */
 package mg.emit;
 
-public class App {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class App extends Application {
     public String getGreeting() {
         return "Hello World!";
     }
 
-    public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+    @Override
+    public void start(Stage stage) {
+        String javaVersion = System.getProperty("java.version");
+        String javafxVersion = System.getProperty("javafx.version");
+        Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
+        Scene scene = new Scene(new StackPane(l), 640, 480);
+        stage.setScene(scene);
+        stage.show();
     }
+
+    public static void main(String[] args) {
+        launch();
+    }
+
 }
