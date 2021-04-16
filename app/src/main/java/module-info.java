104220d0b8de5ecdd07866009b659a23a7f85a07
Rajaomaria Jaona <rajaomariajaona11@gmail.com>
2021-04-16 23:19:09 +0300

    gradle config

--- app/src/main/java/module-info.java
+++ app/src/main/java/module-info.java
@@ -1,10 +1,14 @@
 module Algo.Data.Structure.IA.app {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.web;
     requires java.desktop;
     requires java.logging;
     requires java.base;
+    requires jdk.jsobject;
 
     opens mg.jaona to javafx.fxml;
+    opens mg.jaona.app.graph to javafx.graphics, javafx.fxml;
+    opens mg.jaona.app.graph.controllers to javafx.fxml;
     exports mg.jaona;
 }
