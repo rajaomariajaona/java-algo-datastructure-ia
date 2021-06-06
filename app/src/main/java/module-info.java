21a3063f32657305b0d50170b9b45b9659005c4d
Rajaomaria Jaona <rajaomariajaona11@gmail.com>
2021-06-06 15:50:54 +0300

    feat: basic gluon mobile interface

--- app/src/main/java/module-info.java
+++ app/src/main/java/module-info.java
@@ -6,6 +6,12 @@ module Algo.Data.Structure.IA.app {
     requires java.logging;
     requires java.base;
     requires jdk.jsobject;
+    requires com.gluonhq.charm.glisten;
+    requires com.gluonhq.attach.statusbar;
+    requires com.gluonhq.attach.display;
+    requires com.gluonhq.attach.util;
+    requires com.gluonhq.attach.lifecycle;
+    requires com.gluonhq.attach.storage;
 
     opens mg.jaona to javafx.fxml;
     opens mg.jaona.app.graph to javafx.graphics, javafx.fxml;
